1a6caf28f9a2c3b5b2b0ff1484a8389635916c5b
CosiiRiggz <mwirigicosmas@live.com>
2019-02-08 04:27:27 +0300

    African developer prototype
    My new website comming soon

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+// hide preloder when site fully loads
+// window event lisetner
+eventListeners();
+function eventListeners(){
+    const ui = new UI();
+    // window load
+window.addEventListener('load', function(){
+    ui.hidepreloader();
+});
+// nav btn
+document.querySelector('.navBtn').addEventListener('click',function(){
+    ui.shownav();
+// control the video
+document.querySelector('.video__switch').addEventListener('click',function(){
+    ui.videocontrols();
+})    
+});
+}
+//  CONSTRUCTOR FUNCTION START
+function UI(){
+}
+// hide preloader
+UI.prototype.hidepreloader = function(){
+    document.querySelector('.preloader').style.display="none";
+}
+// show nav bar
+UI.prototype.shownav = function(){
+    document.querySelector('.nav').classList.toggle('nav__show');
+}
+// play or pause video
+UI.prototype.videocontrols = function(){
+    let btn = document.querySelector('.video__switch-btn');
+    if(!btn.classList.contains('btnSlide')){
+        btn.classList.add('btnSlide');
+        document.querySelector('.video__item').pause();
+    }else{
+        btn.classList.remove('btnSlide')
+        document.querySelector('.video__item').play()
+    }
+
+     
+   
+}
+// END OF CONSTRUCTOR FUNCTION
+
+
+
