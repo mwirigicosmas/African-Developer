84c7feb0e8e5ce69a805219350fc46d57879733f
CosiiRiggz <mwirigicosmas@live.com>
2019-02-11 23:23:46 +0300

    form js app
    form js app

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,31 @@ document.querySelector('.navBtn').addEventListener('click',function(){
 document.querySelector('.video__switch').addEventListener('click',function(){
     ui.videocontrols();
 });  
+// submit form
+document.querySelector('.fill-form').addEventListener('submit',function(event){
+event.preventDefault();
+const name = document.querySelector('.input-name').value;
+const lastName = document.querySelector('.input-lastname').value;
+const email = document.querySelector('.input-email').value;
+
+let value = ui.checkEmpty(name, lastName, email);
+
+if(value){
+    let customer = new Customer(name,lastName,email);
+    console.log(customer);
+    ui.addCustomer(customer);
+    ui.showFeedback('successful ','success');
+    ui.clearFields();
+
+
+}else{
+    ui.showFeedback('some form values are empty', 'error');
 }
+
+});
+}
+
+
 //  CONSTRUCTOR FUNCTION START
 function UI(){
 }
@@ -38,6 +62,63 @@ UI.prototype.videocontrols = function(){
         document.querySelector('.video__item').play()
     } 
 }
+UI.prototype.checkEmpty = function(name,lastName,email){
+    let result;
+    if(name === '' || lastName === '' || email === ''){
+        result = false;
+    }else{
+        result = true;
+    }
+    return result;
+}
+UI.prototype.showFeedback = function(text, type){
+    const feedback = document.querySelector('.fill-form__feedback');
+    if(type === 'success'){
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+}else if(type === 'error'){
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
+    }
+}
+// Remove Alert
+UI.prototype.removeAlert = function(type){
+    setTimeout(function () {
+        document.querySelector('.fill-form__feedback').classList.remove(type);   
+    }, 3000);
+
+}
+
+// object constructor to add customer
+UI.prototype.addCustomer = function(customer){
+    const images = [1,2,3,4,5];
+    let random = Math.floor(Math.random()*images.length);
+    const div = document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML =  `
+    <img src="img/${random}.jpg" alt="" class="person__thumbnail">
+                        <h4 class="person__name">${customer.name}</h4>
+                        <h4 class="person__lastname">${customer.lastName}</h4>
+    `
+    document.querySelector('.testimonial-card__list').appendChild(div);
+}
+// clear fields 
+UI.prototype.clearFields = function(){
+    document.querySelector('.input-name').value = '';
+    document.querySelector('.input-lastname').value = '';
+    document.querySelector('.input-email').value = '';
+}
+
+// object constructor function for form
+
+function Customer(name,lastName,email){
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+}
+
 // END OF CONSTRUCTOR FUNCTION
 
 
